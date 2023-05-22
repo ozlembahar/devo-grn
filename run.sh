50a9b66c47ef0fac575c7fd643c3dfdff134fb92
danabarilan <danabr93@gmail.com>
2023-05-22 16:04:35 +0200

    to test run

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-source /home/ybahar/miniconda3/bin/activate devo-grn  && ./script.py 
+source /home/ybahar/miniconda3/bin/activate devo-grn  && python script.py 
 wait
 echo "script done" | mail -s "script done" danabr93@gmail.com
